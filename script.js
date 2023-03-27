47b25cd44d9dd868403a117b89edfb770893b1b8
wbelleau <wibel16@ulaval.ca>
2023-03-27 16:48:24 -0400

    test de sync

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const endpoint = "https://api.imgflip.com/get_memes";
 const memeElement = document.getElementById("meme-image");
 
+//test
 fetch(endpoint)
     .then(response => response.json())
     .then(data => {
