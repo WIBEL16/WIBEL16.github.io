bbcbcc6062b12f9e315192f5b0775fbf7970ea12
wbelleau <wibel16@ulaval.ca>
2023-03-27 11:45:39 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const endpoint = "https://api.imgflip.com/get_memes";
+const memeElement = document.getElementById("meme-image");
+
+fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+        const memes = data.data.memes;
+        const randomIndex = Math.floor(Math.random() * memes.length);
+        const randomMeme = memes[randomIndex];
+
+        memeElement.src = randomMeme.url;
+    })
+    .catch(error => console.error(error));
